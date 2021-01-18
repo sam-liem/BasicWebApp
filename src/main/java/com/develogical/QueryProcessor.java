f6de6c3deda5f57aa20e11789874a0a93900a2b9
Sam Liem <samuelliem@gmail.com>
2021-01-18 16:14:48 +0000

    query processor knows heroku, should deploy

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -7,6 +7,11 @@ public class QueryProcessor {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
+        } else if (query.toLowerCase().contains("heroku")) {
+            return "Heroku is a cloud platform as a service supporting several programming languages." +
+                    "One of the first cloud platforms, Heroku has been in development since June 2007," +
+                    " when it supported only the Ruby programming language, but now supports Java," +
+                    "Node.js, Scala, Clojure, Python, PHP, and Go.";
         }
         return "";
     }
